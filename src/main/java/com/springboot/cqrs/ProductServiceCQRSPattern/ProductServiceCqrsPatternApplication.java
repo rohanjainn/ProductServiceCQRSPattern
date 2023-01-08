7920c5adc9b074ebd8b8d6bcc3ce719f27fa381e
rohanjainn <iamrohanjainn@gmail.com>
2023-01-08 15:33:42 +0530

    initial commit

---
+++ src/main/java/com/springboot/cqrs/ProductServiceCQRSPattern/ProductServiceCqrsPatternApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.cqrs.ProductServiceCQRSPattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceCqrsPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceCqrsPatternApplication.class, args);
+	}
+
+}
